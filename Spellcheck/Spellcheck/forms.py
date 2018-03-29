4d66cd867c979dc1cfbc82484083c4b65199ef0c
denisrogovoy <denis@knu.ua>
2018-03-29 12:32:48 +0300

    Added LogIn and SignUp form

--- Spellcheck/Spellcheck/forms.py
+++ Spellcheck/Spellcheck/forms.py
@@ -14,7 +14,7 @@ class UsersForm(ModelForm):
         exclude={""}
         extra_field = forms.CharField(label='Name of Institution')
         widgets = {
-            'email': forms.TextInput(attrs={'placeholder': 'Input e-mail','required': True,'type': 'email'}),
+            'email': forms.TextInput(attrs={'placeholder': 'Input e-mail','required': True,'type': 'email','autofocus':True}),
             'name': forms.TextInput(attrs={'placeholder': 'Input name', 'required': True}),
             'password': forms.TextInput(attrs={'placeholder': 'Input password','required': True, 'type': 'password'}),
 
