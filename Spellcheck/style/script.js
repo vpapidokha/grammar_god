8b41119ce7a8e93411f9ea423638e2e68b841d93
denisrogovoy <denis@knu.ua>
2018-04-22 03:10:21 +0300

    Made some changes in design

--- Spellcheck/style/script.js
+++ Spellcheck/style/script.js
@@ -19,35 +19,65 @@ $(document).ready(function(){
                 console.log("OK");
                 console.log(data.checkedText);
                 console.log(typeof data.checkedText);
-                $('#areaForOutput').text(data.checkedText);
+                if(data.checkedText=='True') {
+                    $('#pngIncorrect').hide();
+                    $('#textIncorrect').hide();
+                    $('#pngCorrect').show('slow');
+                    $('#textCorrect').show('slow');
+                    console.log('Show image that correct');
+                } else {
+                    $('#pngCorrect').hide();
+                    $('#textCorrect').hide();
+                    $('#pngIncorrect').show('slow');
+                    $('#textIncorrect').show('slow');
+                    console.log('Show image that correct');
+                }
+                /*$('#areaForOutput').text(data.checkedText);*/
                 if (data.added == 'true') {
                     $(".doSwap").load("main.html .queries");
-
+                    var myCase="";
                     console.log(data.textId);
+
+                    if (data.textChecked == "True" ) {
+                        myCase = '<div class="modalCorrectText">' + data.inputedText + '</div>' +
+                            '<div class="checkOutput"><img class="modalpngOutput" src="style\\Pictures\\happyFace.png" width="100" height="100"></div>'
+                            '<div class="modaltextOutput"> Correct!</div>'
+                    } else {
+                        myCase = '<div class="modalIncorrectText">' + data.inputedText + '</div>' +
+                            '<div class="checkOutput"><img class="modalpngOutput" src="style\\Pictures\\cryingFace.png" width="100" height="100"></div>'
+                            '<div class="modaltextOutput">We detected a typo here!</div>';
+                    }
+                    var url="";
+                    if (data.language == "English") {
+                        url = "style\\Pictures\\usa.png";
+                    } else {
+                        url = "style\\Pictures\\usa.png";
+                    }
+                    console.log(myCase);
                     $('.queries').append('<button type="button" id="'+data.textId+'"class="btn btn-success tagButton" data-toggle="modal"' +
                         'data-target="#Modal'+data.textId+'" data-placement="bottom" title="'+data.checkedText+'">' +
                         data.inputedText+' </button>'+
                         '<div class="modal fade" id="Modal'+data.textId+'" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">'+
                         '<div class="modal-dialog modal-dialog-centered" role="document">'+
-                           ' <div class="modal-content">'+
-                               ' <div class="modal-header">'+
-                       '<span>'+
-                            '<h4 class="modal-title" id="exampleModalLabel">Language: '+data.language+'</h4>'+
-                            '<button type="button" class="close" aria-label="Close" data-dismiss="modal">'+
-                                       '<span aria-hidden="true">&times;</span>'+
-                           '</button>'+
+                        ' <div class="modal-content">'+
+                        ' <div class="modal-header">'+
+                        '<span>'+
+                        '<div class="myModalTitle modal-title" id="exampleModalLabel">Language: '+data.language+' '+
+                        '<img class="flag" src='+url+' width="35" height="35">'+'</div>'+
+                        '<button type="button" class="close myClose" aria-label="Close" data-dismiss="modal">'+
+                        '<span aria-hidden="true">&times;</span>'+
+                        '</button>'+
                         '</span>'+
-                                '</div>'+
-                                '<div class="modal-body">'+
-                                    'User name: '+data.userName+'<p>'+
-                                    'Inputed text: '+data.inputedText+
-                                '</div>'+
-                               '<div class="modal-footer">'+
-                                    '<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>'+
-                                '</div>'+
-                           '</div>'+
                         '</div>'+
-                    '</div>'
+                        '<div class="modal-body">'+
+                        myCase+
+                        '</div>'+
+                        '<div class="modal-footer">'+
+                        '<button type="button" class="btn btn-primary myCloseButton" data-dismiss="modal">Close</button>'+
+                        '</div>'+
+                        '</div>'+
+                        '</div>'+
+                        '</div>'
 
 
                     );
